dd3a993b30fff78276593ba80c8f2a9a54826ca5
Andrew Helmer <ahelmer7@live.com>
2018-06-26 14:39:33 -0700

    added functions'

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,8 @@
 var myGlobals = {
   allSuspects: [],
   allScenarios: [],
+  allSites:[],
+  allLocations:[],
 };
 
 // Constructors ============================================================================================================
@@ -28,6 +30,15 @@ var Locations = function(city, coordinates, fact, cityImage, sites, questions) {
   this.questions = questions; // pass in as an object (correct, wrong, final round)
   myGlobals.allLocations.push(this);
 };
+var siteOptions = function(siteOption1,siteOption2,siteOption3,siteImage1,siteImage2,siteImage3){
+  this.siteOption1 = siteOption1;
+  this.siteOption2 = siteOption2;
+  this.siteOption3 = siteOption3;
+  this.siteImage1 = siteImage1;
+  this.siteImage2 = siteImage2;
+  this.siteImage3 = siteImage3;
+  myGlobals.allSites.push(this);
+};
 
 // objects ==================================================================================================================
 var narration = {
@@ -48,6 +59,7 @@ var logic = {
   startLocation: '',
   gameDifficulty: '',
 };
+
 
 // gameplay ===================================================================================================================
 // specify starting logic variables
--- scripts/setup.js
+++ scripts/setup.js
@@ -1,2 +1,24 @@
 'use strict';
 
+// suspects ====================================================================
+new Suspects('Brian Nations', 'bio');
+// Heists ================================================================================
+new Heists('All the style has been stolen from your website!');
+//Locations ==============================================================================
+new Locations('Central Seattle',0,'fact','img',myGlobals.allSites[0]);
+new Locations('Seattle Waterfront');
+new Locations('Ballard');
+new Locations('Fremont');
+new Locations('South Seattle');
+new Locations('Bellevue');
+new Locations('Redmond');
+new Locations('Tacoma');
+// site locations =========================================================
+new siteOptions ('Seattle Art Museum','Amazon','Space Needle','random image');//[0]
+new siteOptions('Pike Place','Ferris Wheel', 'Seattle Aquarium','random image');//[1]
+new siteOptions('Ballard Locks','Golden Gardens','Norweigan Museum','random image');//[2]
+new siteOptions('Troll', 'Gas Works Park','Woodlands Park Zoo','random image');//[3]
+new siteOptions('Century Link Field','International District','Safeco Field','random image');//[4]
+new siteOptions('Bellevue Square','Botanical Gardens','Bellevue High School','random image');//[5]
+new siteOptions('Marymoor Park','Microsoft','Nintendo','random image');//[6]
+new siteOptions('Tacoma Dome','Chambers Bay','Tacoma Narrows Bridge','random image');//[7]
