72226308ba1e368a9eb997338b10462cb7c51c41
dules12 <danielgeo@comcast.net>
2018-06-28 19:53:59 -0700

    added fact variables

--- scripts/app.js
+++ scripts/app.js
@@ -254,6 +254,67 @@ var wrongLocationAnswers = {
   wrong17:`Nothing unusual ever happens around here.`,
 };
 
+var centralSeattleFact = {
+  'Recognized around the globe, the space needle was built for the 1962 World\'s Fair, which drew over 2.3 million visitors.'
+};
+
+var seattleWaterFrontFact = {
+  'The Seattle waterfront is most known for Pike Place Market. Thanks to its famouse flying fish, its the world\'s 33rd most visited tourist attraction.'
+};
+
+var ballardFact = {
+  'Located in Northwestern Seattle, Ballard is historically the center of Scandavian culture. Settlers were drawn to the ideal salmon fishing opportunties.'
+};
+
+var fremontFact = {
+  'Now a neighborhood in Northwestern Seattle, Fremont was its own city until 1891.'
+};
+
+var southSeattleFact = {
+  'Seattle\'s International District is comprised of three neighbrohoods, colloquially referred to as Chinatown, Japantown, and Little Saigon.'
+};
+
+var bellevueFact = {
+  'There are several cities with the same name in the US and around the world. It is derived from the French words for "beautiful view".'
+};
+
+var redmondFact = {
+  'Redmond is sometimes referred to as the "bicycle capital of the world". It hosts an annual bike race on city streets.'
+};
+
+var tacomaFact = {
+  'At the time the third longest suspension bridge in the world, the Tacoma Narrows Bridge notoriously collasped in 1940 due to strong wind conditions combined with faulty construction.'
+};
+
+var leavenworthFact = {
+  'Incorporated in 1906, Leavenworth was converted to a Bavarian style village in the 1960s as a project to revitalize the former struggling logging town.'
+};
+
+var wallaWallaFact = {
+  'Incorporated in 1862, Walla Walla was briefly the most populous city in the territory of Washington as a result of a gold rush in Idaho.'
+};
+
+var westCoastFact = {
+  'Forks is a small logging town in the Northwest corner of the state that has gained recent popularity as the setting of the "Twilight" book series.'
+};
+
+var seaTacTukwilaFact = {
+  'Proabably quite obvious to most, the SeaTac name is a combination of the names Seattle and Tacoma. The city is quite small at only 27,000 people as of a 2010 census.'
+};
+
+var universityDistrictFact = {
+  'University District is so named for being the location of the University of Washington, which has been in that location since 1895.'
+};
+
+var bellinghamFact = {
+  'George Vancouver named "Bellingham" after a member of the British Royal Navy, Sir William Bellingham.'
+};
+
+var spokaneFact = {
+  'Spokane is commonly believed to be the birthplace of "Father\'s Day." The first one is said to have been celebrated in 1910.'
+};
+
+
 // =========================================================================================================================
 // Functions
 // =========================================================================================================================
